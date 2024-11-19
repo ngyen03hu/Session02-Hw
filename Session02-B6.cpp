53eae2a61cb311cbc0abe7a9d960ff6a694c3321
ngyen03hu <nguyen33hu@gmail.com>
2024-11-20 00:53:50 +0700

    Add files via upload

---
+++ Session02-B6.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	float PI = 3.14;
+	float r = 4;
+	float C, S;
+	
+	C = 2 * PI * r;
+	printf("chu vi la %0.3f\n",C);
+	 
+	  S = PI * r * r;
+	 printf("dien tich la %0.3f\n",S);
+	 
+	 return 0; 
+} 
