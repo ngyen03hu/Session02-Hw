8b49361264fbe263058255754d97df905b0f26b1
ngyen03hu <nguyen33hu@gmail.com>
2024-11-20 00:33:32 +0700

    Add files via upload

---
+++ Session02-B1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int a = 1, b = 2;// khai bao bien
+	float result;	//kieu so thuc
+	result = a + b; //toan tu cong
+	printf("tong hai so la %.1f\n",result);
+	return 0; 
+}
---
+++ Session02-B2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	// khai bao va khoi tao bien kieu so nguyuen  
+	int so1 = 10;
+	int so2 = 20;
+	 
+	// tính tong cua so1 va so2
+	int tong = so1 + so2;
+	
+	// ket qua cua phep tinh 
+	printf("phep tinh tong là %d\n",tong);
+	
+	return 0; 
+} 
